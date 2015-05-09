8ba69f87004dd11e1341421adf383cb3df92a265
Jimmy Ngu <jimmynguyc@gmail.com>
2015-05-09 19:46:02 +0800

    minitest stub

---
+++ test/ontraport_api/client_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+require 'ontraport_api/client'
+
+describe OntraportApi::Client do
+
+  describe 'Error handling' do
+    it 'when app_id or api_key blank' do
+      proc {
+        OntraportApi::Client.new('','')
+      }.must_raise OntraportApi::Client::InvalidAppIdOrApiKey
+    end
+  end
+
+
+end
